c036c1c58ffefd14b6374fb2e766de79611c0165
monthchatgpt54-blip <monthchatgpt54@gmail.com>
2026-09-07 20:09:02 +0600

    Fix compile error: add missing RingtoneManager import

--- app/src/main/java/com/shimul/fiverrmessagealarm/MainActivity.java
+++ app/src/main/java/com/shimul/fiverrmessagealarm/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
